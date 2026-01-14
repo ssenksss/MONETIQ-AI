48de79241271b25780172ae53e5dd26b3f3d57a7
devKsenija <152201544+ssenksss@users.noreply.github.com>
2026-01-14 03:09:33 +0100

    Add backend skeleton with Free, Premium, Ultra Premium folders and files

---
+++ MONETIQ/src/main/java/com/monetiq/MonetiqApplication.java
@@ -0,0 +1,13 @@
+package com.monetiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonetiqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonetiqApplication.class, args);
+    }
+
+}
