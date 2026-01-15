98a7250925039bbf5182a642e22357c58186c7e0
devKsenija <152201544+ssenksss@users.noreply.github.com>
2026-01-15 16:43:22 +0100

    Frontend Vue: full redesign, landing, pricing, header/footer, config files

--- MONETIQ/frontend-vue/vite.config.ts
+++ MONETIQ/frontend-vue/vite.config.ts
@@ -1,18 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+// @ts-ignore
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import * as path from 'path';
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
